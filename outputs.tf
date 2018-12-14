ae05a1f4dab7d93062b7c11ad6d1db027107ba76
Mathieu Debove <debovemathieu@gmail.com>
2018-12-14 22:21:25 +0100

    Remove IAM configuration

--- outputs.tf
+++ outputs.tf
@@ -3,16 +3,6 @@ output "service_name" {
   value       = "${aws_ecs_service.default.name}"
 }
 
-output "service_role_arn" {
-  description = "ECS Service role ARN"
-  value       = "${aws_iam_role.ecs_service.arn}"
-}
-
-output "task_role_arn" {
-  description = "ECS Task role ARN"
-  value       = "${aws_iam_role.ecs_task.arn}"
-}
-
 output "service_security_group_id" {
   description = "Security Group ID of the ECS task"
   value       = "${aws_security_group.ecs_service.id}"
